75b7b950a4ce92a5719415e910706c84384ac3d7
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-10-20 21:55:49 +0630

    Add function in shop controller

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,13 +1,17 @@
 class ShopsController < ApplicationController
   def index
+    @shops = Shop.all
   end
 
   def show
+    @shop = Shop.includes(:foods).find(params[:id])
+    @categories = Category.all
+    @foods = @shop.foods
   end
   
   def new
   end
-  
+
   def create
   end
 
